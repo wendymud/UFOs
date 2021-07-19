c5e04758930c7e0dbd609e26a54485955773f5b6
Wendy Mudzinski <wendymud@gmail.com>
2021-07-18 19:19:28 -0700

    updated app.js and index.html

--- js/app.js
+++ js/app.js
@@ -1,41 +1,27 @@
-//import the data from data.js
-const tableDate = data;
+// from data.js
+const tableData = data;
 
-//Reference the HTML table using d3
+// get table references
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");
-}
+  // First, clear out any existing data
+  tbody.html("");
 
-data.forEach((dataRow) => {
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
     let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
-      }
-    );
-  });
-
-  function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
-  
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-      // Append a row to the table body
-      let row = tbody.append("tr");
-  
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-      );
     });
-  }
+  });
+}
 
   function handleClick() {
     // Grab the datetime value from the filter
@@ -61,4 +47,3 @@ data.forEach((dataRow) => {
   
   // Build the table when the page loads
   buildTable(tableData);
-  
